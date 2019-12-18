0c4fbfb55922ead4fe72032585377b5ae2539cab
simtek2018 <49326376+simtek2018@users.noreply.github.com>
2019-12-18 17:59:30 -0500

    up to game manager

---
+++ Assets/MyScript/Damage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField] float damageAmount = 10;
+
+    private void OnCollisionEnter(Collision collision) {
+        Health health = collision.gameObject.GetComponent<Health>();
+        if (health) {
+            health.ApplyDamage(damageAmount);
+        }
+    }
+}
---
+++ Assets/MyScript/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float hitpoints = (float)3.0;
+    [SerializeField] GameObject explosivePrefab;
+
+    void Update()
+    {
+        if (hitpoints <= 0) {
+            if (explosivePrefab) {
+                Instantiate(explosivePrefab, transform.position, transform.rotation);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    public void ApplyDamage(float amount) {
+        hitpoints -= amount;
+    }
+}
